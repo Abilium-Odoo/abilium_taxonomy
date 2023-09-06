640aee8cb91c828ff85f9b1972388e1d583379da
Joel Hari <joel.hari@abilium.com>
2023-09-06 11:39:11 +0200

    added systemtag field #SP2-39

--- taxonomy/models/taxonomy_tag.py
+++ taxonomy/models/taxonomy_tag.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 import logging
 
 
@@ -11,14 +12,21 @@ class TaxonomyTag(models.Model):
     _description = 'Taxonomy Tag'
 
     name = fields.Char(string='Name', required=True, index=True)
-    active = fields.Boolean(string='Archiviert', default=True)
     parent_id = fields.Many2one('taxonomy.tag', string='Parent')
+    systemtag = fields.Boolean(string='System Tag', default=False, help='System Tags können nicht gelöscht werden.')
 
     all_parent_ids = fields.Many2many('taxonomy.tag', string='All Parent Tags', compute='_compute_all_parent_ids',
                                       relation='all_parent_child_tag_rel', column1='child_id', column2='parent_id',
                                       store=True, readonly=True, recursive=True)
     all_child_ids = fields.Many2many('taxonomy.tag', string='All Child Tags', readonly=True,
                                      relation='all_parent_child_tag_rel', column1='parent_id', column2='child_id')
+
+    def unlink(self):
+        systemtags = self.filtered(lambda r: r.systemtag)
+        if systemtags:
+            tag_names = '\n'.join([f'- {tag.name}' for tag in systemtags])
+            raise ValidationError(f'Folgende Tags sind System Tags und können deshalb nicht gelöscht werden:\n{tag_names}')
+        return super().unlink()
 
     def name_get(self):
         res = []
